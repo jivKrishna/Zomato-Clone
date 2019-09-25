2207feb21878875af7092106d8adcdf9ce80fe02
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-09-25 23:41:44 +0530

    omniauth.rb updated!

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,6 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET']
-
-  # Google authentication was problematic at first
-  # I had to provide the redirect_uri here
-
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_sope: 'email profile',
-    access_type: 'online',
-    setup: (lambda do |env|
-      request = Rack::Request.new(env)
-      env['omniauth.strategy'].options['token_params'] = {:redirect_uri => 'http://127.0.0.1:3000/auth/google_oauth2/callback'}
-    end)}
+Rails.application.config.middleware.use OmniAuth::Builder do 
+  #for facebook
+  provider :facebook, AUTH_DETAILS["FACEBOOK_APP_ID"], AUTH_DETAILS["FACEBOOK_APP_SECRET"]
+  #for google_oauth2
+  provider :google_oauth2, AUTH_DETAILS["GOOGLE_CLI_ID"], AUTH_DETAILS["GOOGLE_CLI_SECRET"]
 end
