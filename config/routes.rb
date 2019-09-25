1b02fe20f0888ff18b28ef10391e9b0ddcbfef9f
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-09-26 00:06:21 +0530

    routes for callbacks is created!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
   resources :users
+  get "auth/:provider/callback", to: "sessions#create"
+  delete "sign_out", to: "sessions#destroy", as: "sign_out"
 end
