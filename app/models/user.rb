28c409e4af6eee6c8823bfc88c366cc0c93fa71a
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-09-26 00:29:28 +0530

    method created for checking user already available on site or not then create!

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :full_name, length: {minimum: 3, message: "should be minimum 3 characters long!"}, unless: ->{ full_name.blank? }
   validates :email, uniqueness: true
 
-  def sel.sign_in_from_omniauth(auth)
+  def self.sign_in_from_omniauth(auth)
     find_by(provider: auth["provider"], uid: auth["uid"]) || create_user_from_omiauth(auth)
   end
 
