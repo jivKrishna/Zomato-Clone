66361cdec158f3c598bac316ed2ce46214b1e515
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-09-25 23:38:57 +0530

    schema updated!

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,9 @@ ActiveRecord::Schema.define(version: 2019_09_24_142139) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "full_name"
     t.string "email"
+    t.text "image"
+    t.string "provider"
+    t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
