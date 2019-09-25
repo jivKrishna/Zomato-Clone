d3a0d3ce61607097c9b29a5df215b4c5adaf15b9
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-09-26 00:03:08 +0530

    create_user or signed_by method!

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,18 @@ class User < ApplicationRecord
   validates :full_name, :email, presence: true
   validates :full_name, length: {minimum: 3, message: "should be minimum 3 characters long!"}, unless: ->{ full_name.blank? }
   validates :email, uniqueness: true
+
+  def sel.sign_in_from_omniauth(auth)
+    find_by(provider: auth["provider"], uid: auth["uid"]) || create_user_from_omiauth(auth)
+  end
+
+  def create_user_from_omniauth(auth)
+    create(
+      name: auth["info"]["name"],
+      email: auth["info"]["email"],
+      image: auth["info"]["image"],
+      provider: auth["provider"],
+      uid: auth["uid"]
+    )
+  end
 end
