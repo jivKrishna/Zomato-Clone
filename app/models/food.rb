f8ad533aa533733f73b0d1a9fdf470732220f6df
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-10-01 19:31:05 +0530

    food model validated!

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,10 @@
 class Food < ApplicationRecord
   belongs_to :restaurant
-  has_attached_file :image, styles: { medium: "400x400>", thumb: "100x100>" }
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 50, mesaage: "Minimum 50 chars needed!" } 
+  validates :price, :veg, presence: true
+
+  has_attached_file :image, styles: { large: "300x760>", medium: "300x600>", thumb: "200x200#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
