9d1f3ca08f031641b636b7e40fe56a94401bf479
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-10-01 15:37:17 +0530

    food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,17 @@
 class FoodsController < ApplicationController
-  def new
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @food = @restaurant.foods.create(food_params)
+
+    if @food.save
+      redirect_to @restaurant, flash: { success: "#{@food.name} is added successfully!" }
+    else
+      render "restaurants/show"
+    end
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :description, :price, :veg)
   end
 end
