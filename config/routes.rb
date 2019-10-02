12cc1ec021a2f2b5153845eadb094fd1aa72414a
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-10-02 14:52:07 +0530

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   root "home#index"
   resources :users, :restaurant_categories
 
-  resources :restaurants do 
+  resources :restaurants, only: [:show, :order_food, :new, :create] do 
     resources :foods
   end
+
+  get "/orderfood", to: "restaurants#order_food", as: "/orderfood"
   
   get "admin_registration", to: "users#admin_registration"
   get "profile", to: "users#show"
