83eb3301bd9d99db58e2b4d72722bf76d842083b
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-10-02 17:35:58 +0530

    food model updated!

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,8 +1,11 @@
 class Food < ApplicationRecord
+  belongs_to :food_category
   belongs_to :restaurant
 
+  enum veg: [:veg, :egg, :non_veg]
+
   validates :name, presence: true, length: { minimum: 3 }
-  validates :description, presence: true, length: { minimum: 50, mesaage: "Minimum 50 chars needed!" } 
+  validates :description, presence: true, length: { minimum: 30, mesaage: "Minimum 30 chars needed!" } 
   validates :price, :veg, presence: true
 
   has_attached_file :image, styles: { large: "300x760>", medium: "300x600>", thumb: "200x200#" }
