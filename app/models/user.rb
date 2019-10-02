489751680a5b4eb18a1f2d9d03bf84a42cab7e1d
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-10-03 03:23:39 +0530

    user view and model updated!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   before_save { self.email = email.downcase }
