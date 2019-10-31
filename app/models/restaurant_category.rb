d207b41ea7c05cd1da9ed85c16f64ac92c771885
Krishna Pandit <krishnapandit@localhost.localdomain>
2019-11-01 01:09:10 +0530

    Final commit!

--- app/models/restaurant_category.rb
+++ app/models/restaurant_category.rb
@@ -1,5 +1,5 @@
 class RestaurantCategory < ApplicationRecord
-  has_many :restaurants, dependent: :delete_all
+  has_many :restaurants, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 3, message: "only allows valid emails" },
              uniqueness: { case_sensitive: false }
