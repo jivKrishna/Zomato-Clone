ed1e68caf59fc734c54328d4b4d79cff50f7e63c
KRISHNA PANDIT <krishnapandit@localhost.localdomain>
2019-11-05 23:34:35 +0530

    View updated!

---
+++ app/models/table.rb
@@ -0,0 +1,27 @@
+class Table < ApplicationRecord
+  belongs_to    :user
+  belongs_to    :restaurant
+
+  before_save   :format_book_time
+  before_update :format_book_time
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  before_save { self.guest_email = guest_email.downcase }
+  before_save {self.booking_time = booking_time.strftime("%H:%M")}
+
+  validates :guest_number,       :booking_date,   :booking_time,   :guest_first_name,  :guest_last_name,
+            :guest_phone_number,  presence: true
+
+  validates :guest_email,   presence: true,   format: { with: VALID_EMAIL_REGEX,
+            message: "only allows valid emails" }
+
+  validates_format_of :guest_phone_number,  with: /\A(\d{10}|\(?\d{3}\)?[-.\s]\d{3}[-.\s]\d{4})\z/,
+                      message: "should be 10 digits long and satisfy Phone Number Format"
+
+
+
+  private
+    def format_book_time
+      self.booking_time = booking_time.strftime("%H:%M")
+    end
+end
